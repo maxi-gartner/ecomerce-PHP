6f75af65a874f82c154343da9130d2c39a1f97c5
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-07-16 23:23:56 -0300

    create repository

---
+++ Views/LaytOuts/footer.php
@@ -0,0 +1,21 @@
+</div>
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+
+  <aside class="control-sidebar control-sidebar-dark">
+  </aside>
+</div>
+
+<script src="Util/Js/jquery.min.js"></script>
+<script src="Util/Js/bootstrap.bundle.min.js"></script>
+<script src="Util/Js/adminlte.min.js"></script>
+<script src="Util/Js/demo.js"></script>
+<script src="Util/Js/bootstrap.min.js"></script>
+</body>
+</html>
+
